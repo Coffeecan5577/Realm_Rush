e809e0ec365d96a38d3f165037aaa4214a55ed4a
Cale Curry <c.curry2210@gmail.com>
2018-06-23 16:06:24 -0500

    created EditorSnap.cs, which can snap cubes to the grid by a specified amount during game runtime.

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Tooltip("Value to snap blocks to in grid.")][Range(1f, 20f)] [SerializeField] private float gridSize = 10f;
+    
+
+    void Update()
+    {
+        SnapCubePosition();
+    }
+
+    private void SnapCubePosition()
+    {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPosition.x, 0, snapPosition.z);
+    }
+}
