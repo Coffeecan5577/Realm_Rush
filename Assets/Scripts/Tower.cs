c9232c9926f5d40391bb20436426dcaf579df102
Cale Curry <c.curry2210@gmail.com>
2018-06-30 18:03:16 -0500

    Towers stop shooting when enemies are destroyed or out of attack range.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,12 +6,49 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] private Transform objectToPan;
     [SerializeField] private Transform targetEnemy;
+    [SerializeField] private float attackRange = 30f;
+    [SerializeField] private ParticleSystem towerBullets;
 
     private GameObject enemy;
+  
+
+    void Start()
+    {
+
+    }
 
 	// Update is called once per frame
 	void Update ()
 	{
-	   objectToPan.LookAt(targetEnemy);
+	    if (targetEnemy)
+	    {
+            objectToPan.LookAt(targetEnemy);
+            FireAtEnemy();
+        }
+	    else
+	    {
+	        Shoot(false);
+	    }
+	   
 	}
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, transform.position);
+        towerBullets = GetComponentInChildren<ParticleSystem>();
+        if (distanceToEnemy <= attackRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = towerBullets.emission;
+        emissionModule.enabled = isActive;
+    }
 }
