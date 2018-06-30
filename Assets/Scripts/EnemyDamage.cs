194533afeee006dcb67e4de54608ce89c31b7d54
Cale Curry <c.curry2210@gmail.com>
2018-06-30 17:15:21 -0500

    Enemies now have HP, and get destroyed when HP is 0.

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] private Collider collisionMesh;
+    [SerializeField] private int hitPoints = 10;
 
 	// Use this for initialization
 	void Start ()
@@ -14,7 +15,22 @@ public class EnemyDamage : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
-        print("I'm hit!");
+        ProcessHit();
+        if (hitPoints <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints -= 1;
+        print("Current hit points are: " + hitPoints);
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
     }
 	
 	
