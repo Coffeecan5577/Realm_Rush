036a50b60fe6e42e4eb8dca816d7293436664245
Cale Curry <c.curry2210@gmail.com>
2018-07-03 18:14:49 -0500

    Enemies now selfdestruct when they reach the player base.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
+
+    [SerializeField] private float movementPeriod = .5f;
+    [SerializeField] private ParticleSystem goalParticle;
+    private EnemyDamage selfDestructSequence;
+    
+
+    // Use this for initialization
+    void Start ()
 	{
 	    Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
 	    var path = pathfinder.GetPath();
 	    StartCoroutine(FollowPath(path));
+	    selfDestructSequence = GetComponent<EnemyDamage>();
 	}
     // Update is called once per frame
 	void Update ()
@@ -23,8 +30,20 @@ public class EnemyMovement : MonoBehaviour
         foreach (var waypoint in path)
         {
             transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(movementPeriod);
         }
         print("Ending patrol");
+        if (selfDestructSequence)
+        {
+            SelfDestruct();
+        }
+    }
+
+    private void SelfDestruct()
+    {
+        var vfx = Instantiate(goalParticle, transform.position, Quaternion.identity);
+        vfx.Play();
+        Destroy(vfx.gameObject, 1f);
+        Destroy(gameObject);
     }
 }
