5e18e8124b757fa397f60cd015de0c8c65fd607f
Cale Curry <c.curry2210@gmail.com>
2018-07-04 15:49:05 -0500

    Score counter has been added, and increments on every enemy spawn.

--- Assets/Scripts/BaseHealth.cs
+++ Assets/Scripts/BaseHealth.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseHealth : MonoBehaviour
 {
 
     [SerializeField] private int baseHealth = 10;
+    [SerializeField] private Text healthText;
+
+    void Start()
+    {
+        healthText.text = baseHealth.ToString();
+    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
         baseHealth -= 1;
-        print("Base health is: " + baseHealth);
+        healthText.text = baseHealth.ToString();
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [Range(0.1f, 120f)] [SerializeField] private float secondsBetweenSpawns = 2f;
+    [SerializeField] private int playerScore = 0;
+    [SerializeField] private int scoreAmount = 10;
     [SerializeField] private EnemyMovement enemyPrefab;
     [SerializeField] private Transform enemyParentTransform;
+    [SerializeField] private Text scoreCounter;
 
 	// Use this for initialization
 	void Start ()
 	{
         StartCoroutine(RepeatedlySpawnEnemies());
-    }
+	    scoreCounter.text = playerScore.ToString();
+	}
 	
             
     IEnumerator RepeatedlySpawnEnemies()
@@ -22,6 +27,7 @@ public class EnemySpawner : MonoBehaviour
         {
             var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             newEnemy.transform.parent = enemyParentTransform;
+            AddScore();
             yield return new WaitForSeconds(secondsBetweenSpawns);
 
             if (enemyPrefab == null)
@@ -29,5 +35,11 @@ public class EnemySpawner : MonoBehaviour
                 throw new NullReferenceException("Enemy does not exist.");
             }
         }
+    }
+
+    private void AddScore()
+    {
+        playerScore += scoreAmount;
+        scoreCounter.text = playerScore.ToString();
     }
 }
