5c3353a3a28de70afd98dc019472ee411a27ea04
Cale Curry <c.curry2210@gmail.com>
2018-07-04 15:31:39 -0500

    Player base now has health that decreases from enemy collisions.

---
+++ Assets/Scripts/BaseHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class BaseHealth : MonoBehaviour
+{
+
+    [SerializeField] private int baseHealth = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        baseHealth -= 1;
+        print("Base health is: " + baseHealth);
+    }
+}
